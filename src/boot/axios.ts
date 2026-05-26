fb7609de1e375e78517d203a625b07872ad28e27
Caio Occhi <caioocchi5@gmail.com>
2026-05-26 14:37:17 -0300

    test fixed api url

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -2,10 +2,11 @@ import { boot } from 'quasar/wrappers';
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_URL_API || '',
+  baseURL: 'https://gestor-varandao-api.onrender.com',
 });
 
-console.log(import.meta.env.VITE_URL_API);
+console.log('API URL:', import.meta.env.VITE_URL_API);
+console.log('BASE URL:', api.defaults.baseURL);
 
 export default boot(({ app }) => {
   app.config.globalProperties.$api = api;
