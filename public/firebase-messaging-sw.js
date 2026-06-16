3b62fd67dc897ec67eb8e61dcbc4a0cbed16532b
Caio Occhi <caioocchi5@gmail.com>
2026-06-16 08:14:35 -0300

    teste de correção 3

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,20 +1,19 @@
-/* eslint-disable no-undef */
-importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
+// importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
+// importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
 
 // Parse configuration from registration URL query parameters
-const params = new URL(self.location.href).searchParams;
+// const params = new URL(self.location.href).searchParams;
 
-const firebaseConfig = {
-  apiKey: params.get('apiKey'),
-  authDomain: params.get('authDomain'),
-  projectId: params.get('projectId'),
-  storageBucket: params.get('storageBucket'),
-  messagingSenderId: params.get('messagingSenderId'),
-  appId: params.get('appId'),
-};
+// const firebaseConfig = {
+//   apiKey: params.get('apiKey'),
+//   authDomain: params.get('authDomain'),
+//   projectId: params.get('projectId'),
+//   storageBucket: params.get('storageBucket'),
+//   messagingSenderId: params.get('messagingSenderId'),
+//   appId: params.get('appId'),
+// };
 
-firebase.initializeApp(firebaseConfig);
+// firebase.initializeApp(firebaseConfig);
 // const messaging = firebase.messaging();
 
 // messaging.onBackgroundMessage((payload) => {
