6e194cd186cdac21eb28759bd16efd7030fd5ad0
Caio Occhi <caioocchi5@gmail.com>
2026-06-16 08:05:16 -0300

    teste de correção de notificação

--- src-pwa/custom-service-worker.ts
+++ src-pwa/custom-service-worker.ts
@@ -1,5 +1,5 @@
-import { initializeApp } from 'firebase/app';
-import { getMessaging, onBackgroundMessage } from 'firebase/messaging/sw';
+// import { initializeApp } from 'firebase/app';
+// import { getMessaging, onBackgroundMessage } from 'firebase/messaging/sw';
 
 /*
  * This file (which will be your service worker)
@@ -7,7 +7,8 @@ import { getMessaging, onBackgroundMessage } from 'firebase/messaging/sw';
  * quasar.config file > pwa > workboxMode is set to "InjectManifest"
  */
 
-declare const self: ServiceWorkerGlobalScope & typeof globalThis & { skipWaiting: () => void; registration: ServiceWorkerRegistration };
+declare const self: ServiceWorkerGlobalScope &
+  typeof globalThis & { skipWaiting: () => void; registration: ServiceWorkerRegistration };
 
 import { clientsClaim } from 'workbox-core';
 import {
@@ -20,27 +21,27 @@ import { registerRoute, NavigationRoute } from 'workbox-routing';
 void self.skipWaiting();
 clientsClaim();
 
-const firebaseApp = initializeApp({
-  apiKey: process.env.FIREBASE_API_KEY!,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN!,
-  projectId: process.env.FIREBASE_PROJECT_ID!,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET!,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID!,
-  appId: process.env.FIREBASE_APP_ID!,
-});
+// const firebaseApp = initializeApp({
+//   apiKey: process.env.FIREBASE_API_KEY!,
+//   authDomain: process.env.FIREBASE_AUTH_DOMAIN!,
+//   projectId: process.env.FIREBASE_PROJECT_ID!,
+//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET!,
+//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID!,
+//   appId: process.env.FIREBASE_APP_ID!,
+// });
 
-const messaging = getMessaging(firebaseApp);
+// const messaging = getMessaging(firebaseApp);
 
-onBackgroundMessage(messaging, (payload) => {
-  console.log('Push recebida no Service Worker', payload);
+// onBackgroundMessage(messaging, (payload) => {
+//   console.log('Push recebida no Service Worker', payload);
 
-  void self.registration.showNotification(payload.notification?.title || 'Gestor Varandão', {
-    body: payload.notification?.body || '',
-    icon: '/icons/icon-128x128.png',
-    badge: '/icons/icon-128x128.png',
-    tag: payload.notification?.title || 'gestor-varandao-notification',
-  });
-});
+//   void self.registration.showNotification(payload.notification?.title || 'Gestor Varandão', {
+//     body: payload.notification?.body || '',
+//     icon: '/icons/icon-128x128.png',
+//     badge: '/icons/icon-128x128.png',
+//     tag: payload.notification?.title || 'gestor-varandao-notification',
+//   });
+// });
 
 // Use with precache injection
 precacheAndRoute(self.__WB_MANIFEST);
